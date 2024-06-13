b2cd821d27be481fa7985ca086538a29335715c6
krupangoudsb <67727357+krupan-goud@users.noreply.github.com>
2024-06-13 12:49:33 +0530

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, Container, Row, Col, Button } from 'react-bootstrap'
 import styles from '../styles/pages/Skills.module.css';
 import InputField from '@/components/InputField';
 import { sendUserDetails } from '@/axios';
+import Alert from '../components/Alert';
 
 const initializeUserDetails = {
   name: '',
@@ -13,12 +14,29 @@ const initializeUserDetails = {
 const Contact = () => {
 
   const [userDetails, setUserDetails] = useState<any>(initializeUserDetails);
-
+  const [alertMessage, setAlertMessage] = useState<string>('');
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [validationErrors, setValidationErrors] = useState<any>({
+    name: '',
+    email: '',
+    message: ''
+  });
 
   const onchange = (e: any) => {
     const { name, value } = e.target;
     setUserDetails({ ...userDetails, [name]: value })
-  }
+    if (value.trim() === '') {
+      setValidationErrors({ ...validationErrors, [name]: 'This field is required' });
+    } else if (name === 'name' && value.length < 3) {
+      setValidationErrors({ ...validationErrors, [name]: 'Name must be at least 3 characters long' });
+    } else if (name === 'email' && value.length < 10) {
+      setValidationErrors({ ...validationErrors, [name]: 'Email must be at least 10 characters long' });
+    } else if (name === 'message' && value.length < 20) {
+      setValidationErrors({ ...validationErrors, [name]: 'Message must be at least 20 characters long' });
+    } else {
+      setValidationErrors({ ...validationErrors, [name]: '' });
+    }
+  };
 
   const onSubmitHandler = async () => {
     const data = {
@@ -26,9 +44,19 @@ const Contact = () => {
       email: userDetails.email,
       message: userDetails.message
     }
+    if (userDetails.email.length > 10 && userDetails.name.length > 3 && userDetails.message.length > 20) {
+      const result = await sendUserDetails(data);
+      setAlertMessage(result?.message || 'Message sent successfully');
+      setShowAlert(true);
+    } else {
+      setAlertMessage('Fill required Inputs');
+      setShowAlert(true);
 
-    const result = await sendUserDetails(data);
-    alert("Message Sent Successfully!!!")
+    }
+  }
+
+  const closeAlert = () => {
+    setShowAlert(false);
   }
 
   return (
@@ -38,10 +66,10 @@ const Contact = () => {
           <Col lg={6} md={6} sm={12} xs={12}>
             <Card className={styles.card}>
               <h2 className={`${styles.contactHeading} text-center mb-3`}>Contact</h2>
-              <InputField label={'Full Name'} type={'text'} name={'name'} value={userDetails.name} placeholder={'Enter Full Name'} required={true} rows={0} cols={0} onchange={onchange} />
-              <InputField label={'Email Address'} type={'email'} name={'email'} value={userDetails.email} placeholder={'Enter Email Address'} required={true} rows={0} cols={0} onchange={onchange} />
-              <InputField label={'Message'} type={'textarea'} name={'message'} value={userDetails.message} placeholder={'Hey Krupan, I love your website! I\'d like to discuss some opportunities that you might find interesting! 💐'} required={true} rows={10} cols={12} onchange={onchange} />
-              <Button className={`${styles.hireMeButton} mt-4`} onClick={() => { onSubmitHandler() }}>Send Message</Button>
+              <InputField label={'Full Name'} type={'text'} name={'name'} value={userDetails.name} placeholder={'Enter Full Name'} required={true} rows={0} cols={0} onchange={onchange} error={validationErrors.name} />
+              <InputField label={'Email Address'} type={'email'} name={'email'} value={userDetails.email} placeholder={'Enter Email Address'} required={true} rows={0} cols={0} onchange={onchange} error={validationErrors.email} />
+              <InputField label={'Message'} type={'textarea'} name={'message'} value={userDetails.message} placeholder={'Hey Krupan, I love your website! I\'d like to discuss some opportunities that you might find interesting! 💐'} required={true} rows={10} cols={12} onchange={onchange} error={validationErrors.message} />
+              <Button className={`${styles.hireMeButton} mt-4`} onClick={onSubmitHandler}>Send Message</Button>
               <p className={`${styles.copyRights} mt-4`}>Copyrights © 2024 Krupan Portfolio</p>
               <p className={`${styles.copyRights} mb-4`} > Designed and Developed by Krupan Goud Shivabandhi</p>
             </Card>
@@ -50,6 +78,7 @@ const Contact = () => {
           </Col>
         </Row>
       </Container>
+      {showAlert && <Alert message={alertMessage} onClose={closeAlert} />}
     </div >
   )
 }
