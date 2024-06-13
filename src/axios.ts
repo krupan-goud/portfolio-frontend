915100991a44262ff3a22dc68eb9b66e97b2254a
krupangoudsb <67727357+krupan-goud@users.noreply.github.com>
2024-06-13 12:00:26 +0530

    Update axios.ts

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://" + "localhost:5000" + "/portFolio",
+  baseURL: "http://" + process.env.BACKEND_URL + "/portFolio",
 });
 
 export const sendUserDetails = async (userDetails: any) => {
