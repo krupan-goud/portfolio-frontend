530e108f01e45375d7bdf8bc49b77692c2f33e13
krupangoudsb <67727357+krupan-goud@users.noreply.github.com>
2024-06-13 18:18:11 +0530

    Update skills.tsx

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -28,51 +28,43 @@ const Skills = () => {
           <h3 className={styles.heading}>Skills</h3>
         </div>
         <Row>
-          <Col lg={6} md={8} sm={12} xs={12}>
-            <Container>
-              <div className='mt-4'><h3><FaGears size={36} className='m-3 text-center' />DEVELOPMENT</h3></div>
-              <Row className={`${styles.devLogo} mt-4`}>
-                {skills.map((src, index) => (
-                  <Col key={index} lg={4} md={6} sm={12} xs={12} className={styles.colPadding}>
-                    <div className={styles.imageBorder}>
-                      <Image src={src} alt={'logo'} width={55} height={55} />
-                    </div>
-                  </Col>
-                ))}
-              </Row>
+          <Col lg={6} md={12} sm={12} xs={12}>
+            <div className='mt-4'><h3><FaGears size={36} className='m-3 text-center' />DEVELOPMENT</h3></div>
+            <Row className={`${styles.devLogo} mt-4`}>
+              {skills.map((src, index) => (
+                <Col key={index} lg={4} md={6} sm={12} xs={12} className={styles.colPadding}>
+                  <div className={styles.imageBorder}>
+                    <Image src={src} alt={'logo'} width={55} height={55} />
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+          <Col lg={3} md={12} sm={12} xs={12} className={`${styles.ColMargin} mt-5 mb-5`}>
+            <Container className='mt-5'>
+              <div className='mt-4'><h3><TiSpanner size={36} className='m-3 text-center' />TOOLS</h3>
+                <ul>
+                  <li className={styles.lineStyle}>Git + Github</li>
+                  <li className={styles.lineStyle}>Command Line</li>
+                  <li className={styles.lineStyle}>Chrome DevTools</li>
+                  <li className={styles.lineStyle}>Postman</li>
+                </ul>
+              </div>
             </Container>
+
           </Col>
-          <Col lg={3} md={2} sm={12} xs={12} className={styles.ColMargin}>
-            <Row>
-              <Col lg={12} md={12} xs={12} sm={12} className='mt-5 mb-5'>
-                <Container className='mt-5'>
-                  <div className='mt-4'><h3><TiSpanner size={36} className='m-3 text-center' />TOOLS</h3>
-                    <ul>
-                      <li className={styles.lineStyle}>Git + Github</li>
-                      <li className={styles.lineStyle}>Command Line</li>
-                      <li className={styles.lineStyle}>Chrome DevTools</li>
-                      <li className={styles.lineStyle}>Postman</li>
-                    </ul>
-                  </div>
-                </Container>
-              </Col>
-            </Row>
-          </Col>
-          <Col lg={3} md={2} sm={12} xs={12} className={styles.ColMargin}>
-            <Row>
-              <Col lg={12} md={12} xs={12} sm={12} className='mt-5 mb-5'>
-                <Container className='mt-5'>
-                  <div className='mt-4'><h3><FaLightbulb size={30} className='m-3 text-center' />KNOWLEDGE</h3>
-                    <ul>
-                      <li className={styles.lineStyle}>React Native</li>
-                      <li className={styles.lineStyle}>Data Science</li>
-                      <li className={styles.lineStyle}>ECMAScript 6</li>
-                      <li className={styles.lineStyle}>Material UI</li>
-                    </ul>
-                  </div>
-                </Container>
-              </Col>
-            </Row>
+          <Col lg={3} md={12} sm={12} xs={12} className={`${styles.ColMargin} mt-5 mb-5`}>
+            <Container className='mt-5'>
+              <div className='mt-4'><h3><FaLightbulb size={30} className='m-3 text-center' />KNOWLEDGE</h3>
+                <ul>
+                  <li className={styles.lineStyle}>React Native</li>
+                  <li className={styles.lineStyle}>Data Science</li>
+                  <li className={styles.lineStyle}>ECMAScript 6</li>
+                  <li className={styles.lineStyle}>Material UI</li>
+                </ul>
+              </div>
+            </Container>
+
           </Col>
         </Row>
       </Container>
